d00d1cddacb927fb74f4ee27738edac5826a38e0
Feaxmutch <feaxmutch@gmail.com>
2024-10-22 10:16:00 +0300

    Created Player class

--- Propertys/Program.cs
+++ Propertys/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            
+            Player player = new('@', 5, 5);
+            Console.SetCursorPosition(player.PositionX, player.PositionY);
+            Console.Write(player.Symbol);
+            Console.ReadKey();
         }
+    }
+
+    public class Player
+    {
+        public Player(char symbol, int positionX, int positionY)
+        {
+            Symbol = symbol;
+            PositionX = positionX;
+            PositionY = positionY;
+        }
+
+        public char Symbol { get; private set; }
+
+        public int PositionX { get; private set; }
+
+        public int PositionY { get; private set; }
     }
 }
